747010af808b41e8187d11a49ad4e48f22592131
ClaesF0 <claes.folkestad@stud.noroff.no>
2022-10-16 18:41:03 +0200

    Some weird issue with header

--- js/components/header.js
+++ js/components/header.js
@@ -14,16 +14,16 @@ function createHeaderBar() {
     if (userName) {
       links = `
     <li>
-      <a href="index.html" class="${currentPage === "/index.html" ? "text-white" : ""} block py-2 p-1   bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Feed </a>
+      <a href="index.html" class="${currentPage === "/index.html" ? "text-white" : ""} block py-2 p-1   bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Feed </a>
     </li>
     <!--<li>
-      <a href="make-post.html" class="${currentPage === "/make-post.html" ? "text-white" : ""}block py-2 p-1  text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"> Make new post </a>
+      <a href="make-post.html" class="${currentPage === "/make-post.html" ? "text-white" : ""}block py-2 p-1  text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"> Make new post </a>
     </li>-->
     <li>
-      <a href="my-posts.html" class="${currentPage === "/my-posts.html" ? "text-white" : ""}block py-2 p-1  text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"> My posts </a>
+      <a href="my-posts.html" class="${currentPage === "/my-posts.html" ? "text-white" : ""}block py-2 p-1  text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"> My posts </a>
     </li>
     <li>
-      <a href="profile.html" class="${currentPage === "/profile.html" ? "text-white" : ""}block py-2 p-1  text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"> ${userName}'s profile </a>
+      <a href="profile.html" class="${currentPage === "/profile.html" ? "text-white" : ""}block py-2 p-1  text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"> ${userName}'s profile </a>
     </li>     
 `;
     }
@@ -44,11 +44,12 @@ function createHeaderBar() {
                     <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
                   </svg>
                 </button>
-
               <input type="search" name="search" placeholder="Search" class="bg-blue-800 h-10 px-10 pr-5 w-full rounded-full text-sm focus:outline-none bg-purple-white shadow rounded border-0">
               
           </div>
+          
         <button type="button" id="logout-btn" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center md:mr-0 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Log Out</button>
+        
         </div>
       </nav>`;
   }
@@ -75,4 +76,3 @@ createHeaderBar();
 
 
 export {createHeaderBar};
-
