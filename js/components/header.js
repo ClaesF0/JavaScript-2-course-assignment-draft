53468529cbed7e9cc0e42f438268bb55884039d9
ClaesF0 <claes_folkestad@hotmail.com>
2023-10-02 16:12:54 +0200

    improvements on log in redirection

--- js/components/header.js
+++ js/components/header.js
@@ -9,13 +9,19 @@ function redirectInvalid() {
     location.replace("/signin.html");
   }
 }
-redirectInvalid();
 
 if (!bearerKey) {
-  location.replace("/signin.html");
+  redirectInvalid();
 }
 
 function createHeaderBar() {
+  const bearerKey = getToken();
+
+  // Move the check to the beginning of the function
+  if (!bearerKey) {
+    location.replace("/signin.html");
+    return; // Add a return statement to exit the function
+  }
   const { currentPage } = document.location;
   const navBar = document.getElementById("navBar");
   if (navBar) {
@@ -35,22 +41,22 @@ function createHeaderBar() {
     if (userName) {
       links = `
     <li>
-      <a href="index.html" class="block py-2 p-1  text-white rounded hover:bg-gray-100 md:hover:bg-transparent bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Feed </a>
+      <a href="index.html" class="block py-2 p-1  text-white hover:bg-gray-100 md:hover:bg-transparent bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Feed </a>
     </li>
-      <a href="my-posts.html" class="block py-2 p-1  text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"> My posts </a>
+      <a href="my-posts.html" class="block py-2 p-1  text-white hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"> My posts </a>
     </li>
     <li>
-      <a href="profile.html" class="block py-2 p-1  text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"> ${userName}'s profile </a>
+      <a href="profile.html" class="block py-2 p-1  text-white hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"> ${userName}'s profile </a>
     </li>     
 `;
     }
     navBar.innerHTML = `
-    <nav class="px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <nav class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-gray-200 ">
         <div class="container flex flex-wrap justify-between items-center mx-auto w-4/5">
         <a href="index.html" class="flex items-center">
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Piece of Mind</span>
         </a>
-        <ul class="flex flex-row p-1 mt-1 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <ul class="flex flex-row p-1 mt-1 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
         ${links}
         </ul>
         <div class="relative text-gray-300 w-60 px-2 sm:px-4 py-2.5 mr-2">
@@ -58,7 +64,7 @@ function createHeaderBar() {
                   <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve" width="512px" height="512px">
                     <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
                   </svg>
-                </button>
+                </button>       
               <input type="search" name="search" id="searchInput" placeholder="Search" class="bg-blue-800 h-10 px-10 pr-5 w-full rounded-full text-sm focus:outline-none bg-purple-white shadow border-0">
               <ul class"list-group" id="list"></ul>
               <ul class"list-group" id="listTwo"></ul>
